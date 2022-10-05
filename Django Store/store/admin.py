070e3aacd80334a33083afe2e03da2fff5d7e4ae
iNaoi122 <naosmail.0@gmail.com>
2022-10-05 11:50:19 +0300

    Add frontend cart, shop and checkout

--- Django Store/store/admin.py
+++ Django Store/store/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "digital")
+    list_filter = ("price", "digital")
+
+admin.site.register(Product, ProductAdmin)
+
 admin.site.register(Customer)
-admin.site.register(Product)
+
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAdress)
--- Django Store/store/models.py
+++ Django Store/store/models.py
@@ -8,7 +8,7 @@ class Customer(models.Model):
    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
    name = models.CharField(max_length=200)
    email = models.CharField(max_length=200)
-  
+   
    def __str__(self):
        return self.name
 
